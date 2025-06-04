d11f309783868cc5b3038301a7a1639268ff9086
Sami Abushamat <samshamat600@gmail.com>
2025-06-03 20:09:52 -0500

    some styling changes

--- src/components/Past.jsx
+++ src/components/Past.jsx
@@ -5,7 +5,7 @@ function Past() {
         <div className="past-container">
             <h1 className="past-title"> what ive done</h1>
             <div className='divider'></div>
-            <a href= "https://cs.uic.edu/" className='job' target='_blank' rel='noopener noreferrer'> bachelor of science in cs | UIC </a>
+            <a href= "https://cs.uic.edu/" className='job' target='_blank' rel='noopener noreferrer'> bachelor of science in cs | uic </a>
             <p className='description'> just finished my degree from the unvirsity of illinois at chicago</p>
             <a href= "https://www.passivepossibility.com/" className ='job' target='_blank' rel='noopener noreferrer'> software engineer intern | passivepossibility</a>
             <p className='description'> worked and did some backend work last summer helping users keep track of their properties</p>
